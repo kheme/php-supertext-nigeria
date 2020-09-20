ce2db97299c225f1121d7315f4740633975439fe
Okiemute Omuta <iamkheme@gmail.com>
2020-09-20 22:43:50 +0100

    Clone laravel-supertext-nigeria

---
+++ src/SuperTextNgServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kheme\SuperTextNg;
+
+use Illuminate\Support\ServiceProvider;
+use Kheme\SuperTextNg\Services\SMS;
+
+class SuperTextNgServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes(
+            [
+                __DIR__.'/config/supertextng.php' => $this->app->basePath() . '/config/supertextng.php',
+            ],
+            'config'
+        );
+
+        $this->app->bind(
+            'SMS',
+            function () {
+                return new SMS();
+            }
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
